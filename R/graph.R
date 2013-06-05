8ed36f48421b93b58c6c45919a4921e888aca2c1
Carlo Strub <cs@carlostrub.ch>
2013-06-05 15:58:34 +0200

    comments and export data for tikz

--- R/graph.R
+++ R/graph.R
@@ -1,6 +1,15 @@
 #generate raw data
 system("../bin/ncegm > ../R/raw")
 
+#read data
 data <- read.fwf("raw",skip=9,widths=rep(26,5),col.names = c("a","V","c","aprime","d"))
 
+#plot value function
 plot(data$a,data$V,type="l")
+
+
+# Save data files for use in tikz
+write.table(cbind(data$a,data$V),file="V.data",quote=FALSE,row.names=FALSE,col.names=FALSE)
+write.table(cbind(data$a,data$c),file="c.data",quote=FALSE,row.names=FALSE,col.names=FALSE)
+write.table(cbind(data$a,data$d),file="d.data",quote=FALSE,row.names=FALSE,col.names=FALSE)
+write.table(cbind(data$a,data$aprime),file="aprime.data",quote=FALSE,row.names=FALSE,col.names=FALSE)
